0fe5bab8adecaf6de64a3a811c6274383103b456
Peter Stenman <peter@holo.ag>
2026-06-02 14:57:56 +0200

    feat(ui): add Text component and update exports in UI index

---
+++ apps/web/src/components/ui/text.tsx
@@ -0,0 +1,18 @@
+import type * as React from "react";
+import { cn } from "@/lib/utils";
+
+const sizes = {
+	sm: "text-sm",
+	base: "text-base",
+	xl: "text-xl",
+	lg: "text-lg",
+} as const;
+
+type TextProps = React.PropsWithChildren<{
+	className?: string;
+	size?: keyof typeof sizes;
+}>;
+
+export function Text({ children, className, size = "sm" }: TextProps) {
+	return <p className={cn(sizes[size], className)}>{children}</p>;
+}
